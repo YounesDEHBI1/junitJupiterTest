b9f45a68af2f03afbd30bfc7e47752a49a9896da
YounesDEHBI1 <younesdehbi.n1@gmail.com>
2020-11-27 14:07:00 +0100

    Setting up project and creating a test class

---
+++ junit-5-basics/src/test/java/io/younesdehbi/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package io.younesdehbi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
