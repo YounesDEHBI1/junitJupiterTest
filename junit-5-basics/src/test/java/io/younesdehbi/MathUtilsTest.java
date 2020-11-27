fa91250ab8f3d278ffaddaa71310f444e58b59f2
YounesDEHBI1 <younesdehbi.n1@gmail.com>
2020-11-27 14:14:51 +0100

    Check for valid test by removing fail method and adding my instruction

--- junit-5-basics/src/test/java/io/younesdehbi/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/younesdehbi/MathUtilsTest.java
@@ -1,14 +1,12 @@
 package io.younesdehbi;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		System.out.println("This test runs");
 	}
 
 }
