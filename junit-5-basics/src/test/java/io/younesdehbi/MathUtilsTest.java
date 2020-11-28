a1613d12501624e9fd6ea7b0e452a56fe1764953
YounesDEHBI1 <younesdehbi.n1@gmail.com>
2020-11-28 15:43:30 +0100

    Asserting if expected equals actual

--- junit-5-basics/src/test/java/io/younesdehbi/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/younesdehbi/MathUtilsTest.java
@@ -1,12 +1,21 @@
 package io.younesdehbi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
 	@Test
 	void test() {
-		System.out.println("This test runs");
+		// Get our class instance
+		MathUtils mathUtils = new MathUtils();
+		// Set expected result
+		int expected = 1;
+		// Call method to get actual result
+		int actual = mathUtils.add(1, 1);
+		// Hey JUnit, I am asserting that "expected" & "actual" are the same
+		assertEquals(expected, actual);
 	}
 
 }
