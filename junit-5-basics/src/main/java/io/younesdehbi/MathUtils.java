080e0d531ae623c804c27331d6a16aaa82b75c96
YounesDEHBI1 <younesdehbi.n1@gmail.com>
2020-11-28 16:20:55 +0100

    Computing A Circle Area Using Test Driven Developpement

--- junit-5-basics/src/main/java/io/younesdehbi/MathUtils.java
+++ junit-5-basics/src/main/java/io/younesdehbi/MathUtils.java
@@ -2,7 +2,23 @@ package io.younesdehbi;
 
 public class MathUtils {
 
+	/**
+	 *  Addition method
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public int add(int a, int b) {
 		return a + b;
+	}
+	
+	/**
+	 * Compute Circle Area by Test Driven Developpement
+	 * @param radius
+	 * @return
+	 */
+	public double computeCircleArea(double radius) {
+		// Using Math.PI for precision so we can match the test
+		return Math.PI * radius * radius;
 	}
 }
--- junit-5-basics/src/test/java/io/younesdehbi/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/younesdehbi/MathUtilsTest.java
@@ -14,8 +14,16 @@ class MathUtilsTest {
 		int expected = 2;
 		// Call method to get actual result
 		int actual = mathUtils.add(1, 1);
-		// Hey JUnit, I am asserting that "expected" & "actual" are the same 
+		// Hey JUnit, I am asserting that "expected" & "actual" are the same
 		assertEquals(expected, actual, "The add method should add two number");
 	}
-
+	
+	@Test
+	void testComputeCircleArea() {
+		
+		// Get our class instance
+		MathUtils mathUtils = new MathUtils();
+		// Asserting equality
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
+	}
 }
